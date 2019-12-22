14b83e4880fbaacddc3920a47ac69b67be3fb96b
rodrigodalri <rodrigodalri1995@gmail.com>
2019-12-22 16:40:50 -0300

    heatmap on google maps API

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ Preparing your system
 **Download and install Python Modules**<br/>
 `pip3 install gpxpy`<br/>
 `pip3 install gmplot`<br/>
+
+
+**Usage**<br/>
+`python3 soccerHeatmap.py filename.gpx`<br/>
--- soccerHeatmap.py
+++ soccerHeatmap.py
@@ -1,40 +1,40 @@
-#Soccer Heatmap
-
-#import numpy as np
-#import numpy.random
-#import pandas as pd
-#import seaborn as sns
 import matplotlib.pyplot as plt
-#from matplotlib.patches import Arc
-
-import gpxpy
 import gmplot
+import gpxpy
+import sys
 
 if __name__ == "__main__":
 
+    filename = sys.argv[1]
+    path = 'activities/'+filename
+    name = filename.split('.')[0]
     lat = []
     lon = []
 
-    gpx_file = open('Evening_Run.gpx', 'r')
+    gpx_file = open(path, 'r')
     gpx = gpxpy.parse(gpx_file)
 
+    #parsing latitude and longitude
     for track in gpx.tracks:
         for segment in track.segments:
             for point in segment.points:
                 lat.append(point.latitude)
                 lon.append(point.longitude)
 
-
+    #generating the trace figure
     fig = plt.figure(facecolor = '0.1')
     ax = plt.Axes(fig, [0., 0., 1., 1.], )
     ax.set_aspect('equal')
     ax.set_axis_off()
     fig.add_axes(ax)
     plt.plot(lon, lat, color = 'deepskyblue', lw = 0.3, alpha = 0.9)
-    plt.savefig("outputs/trace.png", dpi=600)
-    plt.savefig("outputs/trace.pdf", dpi=600)
+    plt.savefig("plots/"+name+".png", dpi=600)
 
+
+    #generating the heatmap
     gmap = gmplot.GoogleMapPlotter(lat[0], lon[0], 20)
-
     gmap.heatmap(lat, lon)
-    gmap.draw("outputs/heatmap.html")
+
+    gmap.apikey = "insert your key here"
+
+    gmap.draw("heatmaps/"+name+".html")
